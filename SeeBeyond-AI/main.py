c8cddf8834b9f4b23dbb106c96056806abde8ae5
Miki-Palmisano <150619316+Miki-Palmisano@users.noreply.github.com>
2023-11-30 20:44:15 +0100

    First Update

---
+++ SeeBeyond-AI/main.py
@@ -0,0 +1,7 @@
+#Dataset contiene le immagini degli oggetti da riconoscere, divisi per (train, val, test) e classe (car, cat)
+#training contiene tutti gli script per il training
+#model contiene il modello addestrato
+
+#cat dataset at URL https://www.kaggle.com/datasets/crawford/cat-dataset/data
+#car dataset at URL https://www.kaggle.com/datasets/prondeau/the-car-connection-picture-dataset
+#
